a07428077a69b9d6c8af99f60a1cf3b85cce9f27
zhaoting <zhaoting@V-ZHAOTING-PC>
2020-03-14 16:29:49 +0800

    微信下单，店铺id

--- Ar.IServices/IRecordsOfConsumptionService.cs
+++ Ar.IServices/IRecordsOfConsumptionService.cs
@@ -18,7 +18,7 @@ namespace Ar.IServices
         bool InsertRecore(string typeCode, string userCode, decimal? recordsMoney, string explain, decimal? recordsdonationAmount, decimal? recordsaccountPrincipal, bool IsRecharging = true );
 
         string PayOrder(string productCode, string userCode, string peopleCount, DateTime dateTime, decimal money, string storeId, string orderCode = "" ,string couponCode = "");
-        Order WxPayOrder(string productCode, string userCode, string peopleCount, DateTime dateTime, decimal money, string wxPrepayId, string orderCode = "", string couponCode = "");
+        Order WxPayOrder(string productCode, string userCode, string peopleCount, DateTime dateTime, decimal money, string wxPrepayId, string storeId, string orderCode = "", string couponCode = "");
         Order WxPayNoMoneyOrder(string productCode, string userCode, string peopleCount, DateTime dateTime, decimal money, string orderCode = "", string couponCode = "");
         /// <summary>
         /// 核销
